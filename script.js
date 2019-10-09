ac9c788a336dd91fc7c34289c9787dbe2174be29
Dillion Megida <megidadillion@gmail.com>
2019-10-09 11:56:18 +0100

    Enable pwa

--- script.js
+++ script.js
@@ -55,3 +55,15 @@ userInput.addEventListener('keyup', (e) => {
         addList();
     }
 });
+
+
+// Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+   navigator.serviceWorker.register('sw.js').then( () => {
+    console.log('Service Worker Registered')
+   }).catch(error => {
+    console.log(error);
+   })
+ })
+}
