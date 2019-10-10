8bb6b6e3714a8e31f0a6735112e813f4a7beea2d
Dillion Megida <megidadillion@gmail.com>
2019-10-10 14:35:05 +0100

    new list at the top

--- script.js
+++ script.js
@@ -28,27 +28,31 @@ let message = document.querySelector('#message');
 //initialize the lists in listarray on the screen
 listsFromArray(listArray, lists);
 
+function animMsg(msg, color) {
+	message.innerHTML = msg;
+	message.style.backgroundColor = color;
+	message.style.transform = 'translateY(0)';
+    setTimeout ( () => {
+    	message.style.transform = 'translateY(-45px)';
+    }, 2000 );
+}
+
 // This function adds lists using the listsfromArray function
 function addList() {
     userInput = document.getElementById('userInput').value;
     if(userInput === '') {
-        message.innerHTML = 'Please insert a list before adding &#128683';
-        message.style.backgroundColor = 'coral';
 
-        message.style.transform = 'translateY(0)';
-        setTimeout ( () => {
-        message.style.transform = 'translateY(-40px)';
-        }, 2000 );
+        animMsg('Please insert a list before adding &#128683', '#E27F1C');
+
+    } else if(listArray.filter(list => list === userInput).length > 0) {
+
+    	animMsg('Pause &#128400;, List already exists!', 'purple');
+
     } else {
-        message.innerHTML = 'List successfully added &#128527';
-        message.style.backgroundColor = '#35CE30';
 
-        message.style.transform = 'translateY(0)';
-        setTimeout ( () => {
-        message.style.transform = 'translateY(-40px)';
-        }, 2000 );
+    	animMsg('List successfully added &#128527;', '#50BD41');
 
-        listArray.push(userInput);
+        listArray.unshift(userInput);
         document.getElementById('lists').innerHTML = '';
         listsFromArray(listArray, lists);
         document.getElementById('userInput').value = '';
@@ -64,13 +68,7 @@ function deleteList(listID) {
         list !== listname
     );
 
-    message.innerHTML = 'List successfully deleted &#10060;';
-    message.style.backgroundColor = '#DA3024';
-
-    message.style.transform = 'translateY(0)';
-    setTimeout ( () => {
-    message.style.transform = 'translateY(-40px)';
-    }, 2000 );
+    animMsg('List successfully deleted &#10060;', '#DA3024');
 
     listsFromArray(listArray, lists);
 }
